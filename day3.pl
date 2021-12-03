c521d9d987e465cfc1a448abde0e9f3cd699ebf3
64q <quentin.lebourgeois@gmail.com>
2021-12-03 23:26:06 +0100

    Dirty day 3

--- day3.pl
+++ day3.pl
@@ -25,9 +25,9 @@ load_data(Data, File) :-
 
 b_to_d(0, []).
 
-b_to_d(Result, [Head|Binary]) :-
-    b_to_d(Result1, Binary),
-    length(Binary, Length),
+b_to_d(Result, [Head|Tail]) :-
+    b_to_d(Result1, Tail),
+    length(Tail, Length),
     Result is Result1 + (Head * (2 ** (Length))). 
 
 %
@@ -78,4 +78,44 @@ resolve_p1(Result, File) :-
 % problem 2 - business logic
 %
 
+filter([], [], _, _).
 
+filter([Line|Result], [Line|Data], Value, Index) :-
+    nth0(Index, Line, Value),
+    filter(Result, Data, Value, Index), !. % on evalue pas les autres prédicats.
+
+filter(Result, [_|Data], Value, Index) :-
+    filter(Result, Data, Value, Index).
+
+filter_o2(Data, [Data], _, _).
+
+filter_o2(Result, Data, Gamma, Position) :-
+    nth0(Position, Gamma, Gamma1),
+    filter(Filtered, Data, Gamma1, Position),
+    transpose(Filtered, Transposed),
+    gamma(Gamma2, Transposed),
+    Position1 is Position + 1,
+    filter_o2(Result, Filtered, Gamma2, Position1), !.
+
+filter_co2(Data, [Data], _, _).
+
+filter_co2(Result, Data, Epsilon, Position) :-
+    nth0(Position, Epsilon, Epsilon1),
+    filter(Filtered, Data, Epsilon1, Position),
+    transpose(Filtered, Transposed),
+    gamma(Gamma, Transposed),
+    epsilon(Epsilon2, Gamma),
+    Position1 is Position + 1,
+    filter_co2(Result, Filtered, Epsilon2, Position1), !.
+
+resolve_p2(Result, File) :-
+    load_data(Data, File),
+    transpose(Data, Transposed),
+    gamma(Gamma, Transposed),
+    filter_o2(O2Value, Data, Gamma, 0),
+    writeln(O2Value),
+    b_to_d(O2, O2Value),
+    epsilon(Epsilon, Gamma),
+    filter_co2(CO2Value, Data, Epsilon, 0),
+    b_to_d(CO2, CO2Value),
+    Result is O2 * CO2, !.
